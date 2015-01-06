91ba28e6d7f693d2e18c53cac1e9e86438801933
Georg Wiese <georgwiese@gmail.com>
2015-01-06 13:19:47 +0100

    Add pom.xml & src files

---
+++ src/main/java/de/hpi/smm/meetup_miner/App.java
@@ -0,0 +1,13 @@
+package de.hpi.smm.meetup_miner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
