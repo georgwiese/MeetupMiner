772ef7d41ab5afd6ce5c0eeb5c888bdb6a2bbcc7
Jan Philipp Sachse <jan.sachse@student.hpi.uni-potsdam.de>
2015-01-12 11:23:04 +0100

    Improve TrendlineSlope algorithm

--- src/main/java/de/hpi/smm/meetup_miner/rsvp_analysis/features/TrendlineSlope.java
+++ src/main/java/de/hpi/smm/meetup_miner/rsvp_analysis/features/TrendlineSlope.java
@@ -20,15 +20,16 @@ public class TrendlineSlope implements AbstractFeature {
 	// based on http://classroom.synonym.com/calculate-trendline-2709.html
 	@Override
 	public double forEvent(Event event, Iterable<Event> pastEvents) {
-		ArrayList<Event> events = createOrderedList(pastEvents);		
+		ArrayList<Event> events = createOrderedList(pastEvents);
+		ArrayList<Long> times = normalizeTime(events);
 		float a = 0, b = 0, b1 = 0, b2 = 0, c = 0, d = 0;
 		for (int i = 0; i < events.size(); i++) {
 			Event currentEvent = events.get(i);
-			a += (i + 1) * currentEvent.getYesRsvpCount();
-			b1 += i;
+			a += times.get(i) * currentEvent.getYesRsvpCount();
+			b1 += times.get(i);
 			b2 += currentEvent.getYesRsvpCount();
-			c += i * i;
-			d += i;
+			c += times.get(i) * times.get(i);
+			d += times.get(i);
 		}
 		a *= events.size();
 		b = b1 * b2;
@@ -43,6 +44,14 @@ public class TrendlineSlope implements AbstractFeature {
 			result.add(event);
 		}
 		Collections.sort(result, new TimeComparator());
+		return result;
+	}
+	
+	private ArrayList<Long> normalizeTime(ArrayList<Event> events) {
+		ArrayList<Long> result = new ArrayList<Long>();
+		for (Event event : events) {
+			result.add(event.getTime() - events.get(0).getTime());
+		}
 		return result;
 	}
 
