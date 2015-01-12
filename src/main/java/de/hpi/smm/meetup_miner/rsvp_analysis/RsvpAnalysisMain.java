3dde1e0b0d1c1db2f957f4a5e4d930193bfca11b
Jan Philipp Sachse <jan.sachse@student.hpi.uni-potsdam.de>
2015-01-12 15:45:45 +0100

    Made RsvpAnalysisMain multithreaded

--- src/main/java/de/hpi/smm/meetup_miner/rsvp_analysis/RsvpAnalysisMain.java
+++ src/main/java/de/hpi/smm/meetup_miner/rsvp_analysis/RsvpAnalysisMain.java
@@ -16,33 +16,86 @@ import de.hpi.smm.meetup_miner.rsvp_analysis.features.TrendlineSlope;
 public class RsvpAnalysisMain {
 	
 	private static final String CITY = "chicago";
+	
+	private static class RsvpAnalysisThread extends Thread {
+
+		private List<Integer> groupIDs;
+		
+		public RsvpAnalysisThread(List<Integer> groupIDs) {
+			this.groupIDs = groupIDs;
+		}
+		
+		@Override
+		public void run() {
+			Connection connection = null;
+			try {
+				connection = DatabaseConnector.getNewConnection();
+				EventLoader eventLoader = new EventLoader(connection);
+				while (true) {
+					int groupId = -1;
+					synchronized (groupIDs) {
+						if (groupIDs.size() > 0) {
+							groupId = groupIDs.remove(0);
+							System.out.println(this.getName() + " - Started new group (" + groupIDs.size() + " remaining)");
+						} else {
+							break;
+						}
+					}
+					List<Event> allEvents = eventLoader.load(Integer.toString(groupId));
+		    		List<Event> pastEvents = new ArrayList<>();
+		    		List<Event> upcomingEvents = new ArrayList<>();
+		    		splitEvents(allEvents, pastEvents, upcomingEvents);
+		    		System.out.println(this.getName() + "\tGroup " + groupId + " - upcoming: " + upcomingEvents.size() + "; past: " + pastEvents.size());
+		    		
+		    		for (int j = 0; j < upcomingEvents.size(); j++) {
+		    			Event event = upcomingEvents.get(j);
+		    			System.out.println("\t" + this.getName() + "\tEvent " + j + " of " + upcomingEvents.size());
+		    			event.expectedSize = (float) (new ExpectedSize()).forEvent(event, pastEvents);
+		    			event.expectedMemberLoyality = (float) (new ExpectedMemberLoyality()).forEvent(event, pastEvents);
+		    			event.expectedTrend = (float) (new TrendlineSlope()).forEvent(event, pastEvents);
+		    			
+		    			event.saveToDatabase(connection);
+		    		}
+				}
+			} catch (ClassNotFoundException | SQLException e) {				
+				e.printStackTrace();
+			} finally {
+				if (connection != null) {
+					try {
+						DatabaseConnector.closeConnection(connection);
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+			System.out.println(this.getName() + " exited.");
+			super.run();
+		}
+		
+	}
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
     	Connection connection = DatabaseConnector.getNewConnection();
-    	Connection saveConnection = DatabaseConnector.getNewConnection();
-    	GroupIdLoader groupIdLoader = new GroupIdLoader(connection);
-    	EventLoader eventLoader = new EventLoader(connection);    	
-    	
+    	GroupIdLoader groupIdLoader = new GroupIdLoader(connection);    	    
+     	
     	List<Integer> groupIDs = groupIdLoader.load(CITY);
-    	for (int i = 0; i < groupIDs.size(); i++) {
-    		int groupId = groupIDs.get(i);    		
-        	List<Event> allEvents = eventLoader.load(Integer.toString(groupId));
-    		List<Event> pastEvents = new ArrayList<>();
-    		List<Event> upcomingEvents = new ArrayList<>();
-    		splitEvents(allEvents, pastEvents, upcomingEvents);
-    		System.out.println("Group " + i + " of " + groupIDs.size() + " (upcoming: " + upcomingEvents.size() + "; past: " + pastEvents.size() + ")");
-    		
-    		for (int j = 0; j < upcomingEvents.size(); j++) {
-    			Event event = upcomingEvents.get(j);
-    			if (j % 10 == 0)
-    				System.out.println("\tEvent " + j + " of " + upcomingEvents.size());
-    			event.expectedSize = (float) (new ExpectedSize()).forEvent(event, pastEvents);
-    			event.expectedMemberLoyality = (float) (new ExpectedMemberLoyality()).forEvent(event, pastEvents);
-    			event.expectedTrend = (float) (new TrendlineSlope()).forEvent(event, pastEvents);
-    			
-    			event.saveToDatabase(saveConnection);
-    		}
+    	ArrayList<Thread> threads = new ArrayList<Thread>();
+    	int numberOfThreads = 8;
+    	for (int i = 0; i < numberOfThreads; i++) {
+    		RsvpAnalysisThread thread = new RsvpAnalysisThread(groupIDs);
+    		threads.add(thread);
+    		thread.start();
     	}
+    	
+    	for (Thread thread : threads) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	private static void splitEvents(
